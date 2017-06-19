dcab2901a703e8e86ac87173f57c30fd225ba059
Eelco Wiersma <eelco@pagebakers.nl>
2017-06-19 11:45:36 +0200

    Initial commit

---
+++ dist/js/craftstatt.js
@@ -0,0 +1,19 @@
+jQuery(function() {
+  $('#contact-form form').submit(function(e) {
+    e.preventDefault()
+
+    var data = {
+      name: $('#name').val(),
+      email: $('#email').val(),
+      description: $('#message').val()
+    }
+
+    analytics.identify(data.email, data)
+
+    try {
+      localStorage.setItem('craftsman', JSON.stringify(data))
+    } catch (e) { console.error(e) }
+
+    $('#contact-form').html('<div class="8u 12u$(small)">Awesome! ' + data.name + ', thanks for your interest.</div>')
+  })
+})
